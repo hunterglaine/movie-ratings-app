a0c116caba0c26ce194e98b14c3791b751016cb9
Hunter Laine <hunterglaine@gmail.com>
2021-02-03 17:56:31 -0800

    Add script to seed movies in database

--- seed_database.py
+++ seed_database.py
@@ -8,3 +8,27 @@ from datetime import datetime
 import crud
 import model
 import server
+
+os.system('dropdb ratings')
+os.system('createdb ratings')
+
+model.connect_to_db(server.app)
+model.db.create_all()
+
+with open('data/movies.json') as f:
+    movie_data = json.loads(f.read())
+
+# Create movies, store them in list so we can use them
+# to create fake ratings later
+movies_in_db = []
+for movie in movie_data:
+    title = movie.get('title')
+    overview = movie.get('overview')
+    poster_path = movie.get('poster_path')
+    release_str = movie.get('release_date')
+    format = "%Y-%m-%d"
+    release_date = datetime.strptime(release_str, format)
+
+    new_movie = crud.create_movie(title, overview, release_date, poster_path)
+
+    movies_in_db.append(new_movie)
