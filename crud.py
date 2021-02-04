7172ab572d2cc86b77fc8e310391b2225c433874
Hunter Laine <hunterglaine@gmail.com>
2021-02-03 17:13:11 -0800

    Create create_movie function

--- crud.py
+++ crud.py
@@ -12,6 +12,19 @@ def create_user(email, password):
 
     return user
 
+
+def create_movie(title, overview, release_date, poster_path):
+    """Create and return a new movie."""
+
+    movie = Movie(title=title, overview=overview, release_date=release_date,
+                    poster_path=poster_path)
+    
+    db.session.add(movie)
+    db.session.commit()
+
+    return movie
+    
+
 if __name__ == '__main__':
     from server import app
     connect_to_db(app)
