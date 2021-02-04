ec595cad31bda9483c3b46404f05036a7dfbda4b
Hunter Laine <hunterglaine@gmail.com>
2021-02-03 17:14:50 -0800

    Create create_rating function

--- crud.py
+++ crud.py
@@ -23,7 +23,18 @@ def create_movie(title, overview, release_date, poster_path):
     db.session.commit()
 
     return movie
-    
+
+
+def create_rating(user, movie, score):
+    """create and return a new rating"""
+
+    rating = Rating(user=user, movie=movie, score=score)
+
+    db.session.add(rating)
+    db.session.commit()
+
+    return rating
+
 
 if __name__ == '__main__':
     from server import app
